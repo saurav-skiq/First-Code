2c59b49684a63d4855c0ae2bb2d8aa10636392bd
Saurav Kumar Sahoo <sauravkumar2021@vitbhopal.ac.in>
2023-02-05 03:09:37 +0530

    Datatype

---
+++ Datatype.java
@@ -0,0 +1,20 @@
+class Datatype {
+    
+    public static void main(String a[]) {
+
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 58451;
+
+        float f = 5.5f;
+        double d = 5.8;
+
+        char c = 'k';
+
+        boolean b = true;
+        
+    }
+
+
+}
